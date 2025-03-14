4a790261efd6504133f8053cf4c560983cce92ee
Staiger, Christine <staigerchristine@gmail.com>
2025-03-14 16:26:17 +0100

    scale nodes and graph according to degrees

--- surfiamviz/utils.py
+++ surfiamviz/utils.py
@@ -2,6 +2,7 @@
 
 import itertools
 from pathlib import Path
+import math
 
 import gravis as gv
 import networkx as nx
@@ -11,20 +12,28 @@ import tomllib
 def render_editable_network(graph: nx.MultiDiGraph, html_path: Path):
     """Save the graph as html file."""
     print(f"Rendering {html_path}:")
-    # fix hierarchical positioning of nodes
-    pos = nx.drawing.layout.multipartite_layout(graph, scale=300)
+    # fix hierarchical positioning of node
+    max_deg = max([deg for _, deg in graph.to_undirected().degree])
+    scaling = 300 + len(graph.nodes())*max_deg
+    print(scaling)
+    pos = nx.drawing.layout.multipartite_layout(graph, scale=scaling)
     for name, (x, y) in pos.items():
         node = graph.nodes[name]
         node["x"] = x
         node["y"] = y
 
-    fig = gv.d3(
+    deg_centrality = dict(graph.to_undirected().degree)
+    [graph.add_node(node, size=10+deg_centrality[node]) for node in graph.nodes()]
+
+    fig = gv.vis(
         graph,
         show_edge_label=True,
         edge_label_data_source="label",
         edge_curvature=0.3,
-        node_size_normalization_max=50,
+        use_node_size_normalization=False,
+        node_size_data_source='size',
         layout_algorithm_active=False,
+        show_details=True,
     )
     fig.export_html(html_path)
 
