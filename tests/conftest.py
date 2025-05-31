6ab9885a86586adccf9bf4214dfcf740d9dc2366
Staiger, Christine <staigerchristine@gmail.com>
2025-05-31 23:58:27 +0200

    move testdata to tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,13 +7,13 @@ import tomllib
 
 @pytest.fixture(scope="session")
 def config():
-    with open(Path("testdata") / "config.toml", "rb") as handle:
+    with open(Path("tests/testdata") / "config.toml", "rb") as handle:
         config_data = tomllib.load(handle)
     return config_data
 
 
 @pytest.fixture(scope="session")
 def sram():
-    with open(Path("testdata") / "sram.json", "rb") as handle:
+    with open(Path("tests/testdata") / "sram.json", "rb") as handle:
         sram_data = json.load(handle)
     return sram_data
